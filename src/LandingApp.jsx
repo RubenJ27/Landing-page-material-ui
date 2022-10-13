2100ee962bcaf8bf00d5932ad6e763926b795cde
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-10-13 12:59:49 -0500

    Se creo la estructura principal de la landing

---
+++ src/LandingApp.jsx
@@ -0,0 +1,8 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+export const LandingApp = () => {
+  return (
+    <Button variant="contained" color="primary">Hola mundo mui</Button>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { LandingApp } from './LandingApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <LandingApp />
+)
